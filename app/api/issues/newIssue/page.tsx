e296979a94667d9fe5faab286ac73c42e3eeaf2c
amanuel <amanuel2123alex@gmail.com>
2024-12-16 23:13:58 +0300

    hadle form submit

--- app/api/issues/newIssue/page.tsx
+++ app/api/issues/newIssue/page.tsx
@@ -1,13 +1,41 @@
-import { TextField } from "@radix-ui/themes";
-import React from "react";
+"use client";
 
+import React from "react";
+import { useForm, Controller } from "react-hook-form";
+import { Button, TextField } from "@radix-ui/themes";
+import Simplemde from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import Axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 const newIssuespage = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const router = useRouter();
   return (
-    <div className="max-w-xl">
-      <TextField.Root placeholder="Title">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit((data) => {
+        Axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+    >
+      <TextField.Root placeholder="Title" {...register("title")}>
         <TextField.Slot></TextField.Slot>
       </TextField.Root>
-    </div>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <Simplemde placeholder="Description" {...field}></Simplemde>
+        )}
+      />
+
+      <Button>Submit New Issue</Button>
+    </form>
   );
 };
 
