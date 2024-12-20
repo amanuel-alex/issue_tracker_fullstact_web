10c91c1a7c0b02f2445f2bf4537a0cd4a47ab9be
amanuel <amanuel2123alex@gmail.com>
2024-12-20 17:03:34 +0300

    show the issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,15 +1,51 @@
-"use client";
+// "use client";
 
-import { Button } from "@radix-ui/themes";
+import { PrismaClient } from "@prisma/client";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const prisma = new PrismaClient();
+  const issues = await prisma.issue.findMany();
   return (
     <>
-      <Button>
-        <Link href="/api/issues/newIssue">New Issues</Link>
-      </Button>
+      <div className="mb-3">
+        <Button>
+          <Link href="/api/issues/newIssue">New Issues</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issues</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Description</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden">{issue.status}</div>
+              </Table.Cell>
+              <Table.Cell>{issue.description}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
     </>
   );
 };
